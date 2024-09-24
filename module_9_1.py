482d2e36ddf022d42cf7e1378fbf5ebd03bee1a6
AArinova <aliya-arinova@yandex.ru>
2024-09-24 15:58:02 +0700

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,12 @@
+def apply_all_func(int_list: list, *functions):
+
+    result = dict()
+    
+    for i_func in functions:
+        result[ i_func.__name__] =i_func(int_list)
+
+    return(result)
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
