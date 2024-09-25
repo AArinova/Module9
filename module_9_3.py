fc5e4dd760e4021cdaff4a2d83f7ce9353e1a245
AArinova <aliya-arinova@yandex.ru>
2024-09-25 12:26:59 +0700

    Add files via upload

--- module_9_3.py
+++ module_9_3.py
@@ -1,6 +1,5 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
-united = zip( first, second)
 
 first_result = [abs(len(item[0]) - len(item[1])) for item in zip(first, second) if len(item[0]) != len(item[1])]
 second_result = [len(first[ii]) == len(second[ii]) for ii in range(0, len(first))]
