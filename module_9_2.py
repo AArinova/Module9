3f3454da3890e76a8db3ce1abd8568b4dd43efd8
AArinova <aliya-arinova@yandex.ru>
2024-09-25 11:42:42 +0700

    Add files via upload

--- module_9_2.py
+++ module_9_2.py
@@ -3,7 +3,7 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 
 first_result = [len(item) for item in first_strings if len(item)>5]
 second_result = [(item1, item2) for item1 in first_strings for item2 in second_strings if len(item1) == len(item2)]
-third_result = {item: len(item) for item in first_strings + second_strings if (len(item) % 2) == 0}
+third_result = {item: len(item) for item in first_strings + second_strings if not len(item) % 2}
 
 print(first_result)
 print(second_result)
