5d9a31a5a597e63c9ae450115cf3728ee8039326
AArinova <aliya-arinova@yandex.ru>
2024-09-27 12:10:12 +0700

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,15 @@
+def all_variants(text: str):
+    res = set()
+
+    for len_sym in range(1, len(text) + 1):
+        for i in range(len(text)):
+            for j in range(i + 1, len(text) + 1):
+                if len(text[i:j]) == len_sym:
+                    res.add(text[i:j])
+
+    return res
+
+
+a = all_variants("abc")
+for i in a:
+    print(i, end = ' ')
